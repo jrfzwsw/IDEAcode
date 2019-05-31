751a069302b89fc420e4910b510ce8fbdf4421b2
yby <yby@163.com>
2019-05-31 20:17:00 +0800

    六脉神剑

--- mygit/src/main/java/LIumanshenjian.java
+++ mygit/src/main/java/LIumanshenjian.java
@@ -2,6 +2,7 @@ public class LIumanshenjian {
 	public static void main(String[] args) {
 		System.out.println("六脉神剑");
 		System.out.println("凌波微步");
+		System.out.println("无量山");
 		System.out.println("琅環福地");
 	}
 }
